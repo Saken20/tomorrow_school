3d21f9ca44b6645e7a3372c9db1bb3cc3298492b
Guest User <Guest@TCA-16.local>
2025-01-21 15:11:04 +0500

    ;l

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 
-curl https://01.tomorrow-school.ai/assets/superhero/all.json | jq -r ".[] | select(.id == 170) | .name"
+curl https://01.tomorrow-school.ai/assets/superhero/all.json | jq -r ".[] | select(.id == $HERO_ID) | .connections.relatives"
