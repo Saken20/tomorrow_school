f408a5e07c94c28c242058415f4319890eaaf7c8
Guest User <Guest@TCA-03.local>
2025-01-22 19:58:32 +0500

    lol

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for z := i; z <= '9'; z++ {
+				for x := '0'; x <= '9'; x++ {
+					if (i < z) || (i == z && j < x) {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(z)
+						z01.PrintRune(x)
+						if !(i == '9' && j == '8' && z == '9' && x == '9') {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
