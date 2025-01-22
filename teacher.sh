9f0ba3aec50cc103ac4c9ff79b9dd3a90107e0dc
Guest User <Guest@TCB-17.local>
2025-01-22 22:10:25 +0500

    sh

--- teacher.sh
+++ teacher.sh
@@ -2,7 +2,7 @@ interview=$(head -n "179" "streets/Buckingham_Place" | tail -n -1)
 read -a array <<< "$interview"
 export INTERVIEW_NUMBER=$(echo "${array[2]#?}")
 
-interview=$(cat "interview/interview-${INTERVIEW_NUMBER}")
+interview=$(cat "interviews/interview-${INTERVIEW_NUMBER}")
 
 echo "$INTERVIEW_NUMBER"
 echo "$interview"
