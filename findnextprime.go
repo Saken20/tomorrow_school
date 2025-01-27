55745f349d89805cd2be64136d36cdcf14a86976
Guest User <Guest@TCA-15.local>
2025-01-27 21:02:32 +0500

    olo

--- findnextprime.go
+++ findnextprime.go
@@ -1,21 +1,6 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	if nb < 2 {
-		return false
-	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func FindNextPrime(nb int) int {
-	if nb >= 100 {
-		return -1
-	}
 	if IsPrime(nb) {
 		return nb
 	}
