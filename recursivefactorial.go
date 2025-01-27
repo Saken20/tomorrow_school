62c211bf228e9a94520ae0586dd859eccbf65dd9
Guest User <Guest@TCA-15.local>
2025-01-27 19:26:43 +0500

    ollo

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,14 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 1 {
+	if nb < 0 {
 		return 0
 	}
-	if nb > 0  && nb < 1000 {
-		return nb * RecursiveFactorial(nb-1)
+	if nb == 0 || nb == 1 {
+		return 1
 	}
-	return 0
+	if nb > 1000 {
+		return 0
+	}
+	return nb * RecursiveFactorial(nb-1)
 }
