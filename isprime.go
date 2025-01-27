1b30713d1df684503fb917e3fa7ab4340e8f6eec
Guest User <Guest@TCA-15.local>
2025-01-27 20:42:38 +0500

    lolol

--- isprime.go
+++ isprime.go
@@ -1,19 +1,18 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb <= 1 {
+	if nb == 1 {
 		return false
-	}
-	if nb == 2 {
+	} else if nb == 2 {
 		return true
-	}
-	if nb%2 == 0 {
+	} else if nb > 2 {
+		for i := 2; i < nb; i += 2 {
+			if nb%i == 0 {
+				return false
+			}
+		}
+		return true
+	} else {
 		return false
 	}
-	for i := 0; i*i < nb; i += 2 {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
 }
