aa43a0d2ff1a1311ce11e1111a6e7a54e78701be
Guest User <Guest@TCB-17.local>
2025-01-30 03:02:10 +0500

    loo

---
+++ capitalize.go
@@ -0,0 +1,19 @@
+package piscine
+
+func Capitalize(s string) string {
+	a := ToUpper(string(s[0]))
+	if len(s) == 0 {
+		return a
+	} else {
+		for i := 1; i < len(s); i++ {
+			if s[i-1] == ' ' {
+				a += ToUpper(string(s[i]))
+			} else if s[i-1] == '+' {
+				a += ToUpper(string(s[i]))
+			} else {
+				a += string(s[i])
+			}
+		}
+	}
+	return a
+}
