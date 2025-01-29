c4630f19c166287f80c8cc57a86d82e07942b6e6
Guest User <Guest@TCB-17.local>
2025-01-30 04:43:31 +0500

    changes

---
+++ sortparams/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+func main() {
+	var s []string
+	s = append(s, os.Args[1:]...)
+	SortIntegerTable(s)
+	for _, val := range s {
+		Print([]rune(val))
+	}
+}
+func Print(ans []rune) {
+	for i := 0; i < len(ans); i++ {
+		z01.PrintRune(rune(ans[i]))
+	}
+	z01.PrintRune('\n')
+}
+func SortIntegerTable(table []string) {
+	for i := 0; i < len(table); i++ {
+		for j := i + 1; j < len(table); j++ {
+			if []rune(table[j])[0] < []rune(table[i])[0] {
+				table[j], table[i] = table[i], table[j]
+			}
+		}
+	}
+}
