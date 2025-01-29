b85d1e3ab083a94971011b193a07773ca9bd80ac
Guest User <Guest@TCB-17.local>
2025-01-30 04:21:05 +0500

    lolo

--- revparams/main.go
+++ revparams/main.go
@@ -12,10 +12,10 @@ func main() {
 		return
 	}
 
-	args = args[len(args)-1:1]
+	args = args[1:]
 
-	for _, word := range args {
-		for _, ch := range word {
+	for i := len(args)-1; i <= 1; i-- {
+		for _, ch := range args[i] {
 			z01.PrintRune(ch)
 		}
 		z01.PrintRune('\n')
