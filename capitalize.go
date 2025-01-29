ed33a4bfb2f77e90630dd2eee008c1672a14c8a3
Guest User <Guest@TCB-17.local>
2025-01-30 03:28:37 +0500

    loo

--- capitalize.go
+++ capitalize.go
@@ -1,19 +1,41 @@
 package piscine
 
 func Capitalize(s string) string {
-	a := ToUpper(string(s[0]))
+	var t string = ""
 	if len(s) == 0 {
-		return a
+		return t
+	} else if IsLet(s[0]) == 1 {
+		t += string(s[0] - 32)
 	} else {
-		for i := 1; i < len(s); i++ {
-			if (s[i-1] >= '0' && s[i-1] <= '9') || (s[i-1] >= 'A' && s[i-1] <= 'Z') || (s[i-1] >= 'a' && s[i-1] <= 'z') {
-				a += ToLower(string(s[i]))
-			} else if s[i-1] < '0' && s[i-1] > '9' && s[i-1] < 'A' && s[i-1] > 'Z' && s[i-1] < 'a' && s[i-1] > 'z' {
-				a += ToUpper(string(s[i]))
+		t += string(s[0])
+	}
+	for i := 1; i < len(s); i++ {
+		if IsLet(s[i]) == 2 {
+			if s[i-1] != ' ' && IsLet(s[i-1]) != 0 {
+				t += string(s[i] + 32)
 			} else {
-				a += string(s[i])
+				t += string(s[i])
 			}
+		} else if IsLet(s[i]) == 1 {
+			if s[i-1] == ' ' || IsLet(s[i-1]) == 0 {
+				t += string(s[i] - 32)
+			} else {
+				t += string(s[i])
+			}
+		} else {
+			t += string(s[i])
 		}
 	}
-	return a
+	return t
+}
+
+func IsLet(s byte) int {
+	if s >= '0' && s <= '9' {
+		return 3
+	} else if s >= 'A' && s <= 'Z' {
+		return 2
+	} else if s >= 'a' && s <= 'z' {
+		return 1
+	}
+	return 0
 }
