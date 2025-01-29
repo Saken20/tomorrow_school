1cfe35ff42d35bd503367bd0ab7cb8a635ef6382
Guest User <Guest@TCB-17.local>
2025-01-29 22:42:07 +0500

    lol

--- index.go
+++ index.go
@@ -5,7 +5,7 @@ func Index(s string, toFind string) int {
 		return 0
 	}
 	for i := 0; i <= len(s)-len(toFind); i++ {
-		 if s[i:i+len(toFind)] == toFind{
+		 if s[i:i+len(toFind)] == toFind {
 			return i
 		}
 	}
