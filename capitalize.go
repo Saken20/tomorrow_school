025ff18162240d6e01f1a7eff80eb967c3500e3e
Guest User <Guest@TCB-17.local>
2025-01-30 03:10:15 +0500

    loo

--- capitalize.go
+++ capitalize.go
@@ -8,6 +8,8 @@ func Capitalize(s string) string {
 		for i := 1; i < len(s); i++ {
 			if s[i-1] >= 32 && s[i-1] <= 47 {
 				a += ToUpper(string(s[i]))
+			} else if (s[i-1] >= 65 && s[i-1] <= 90) || (s[i-1] >= 48 && s[i-1] <= 57) {
+				a += ToLower(string(s[i]))
 			} else {
 				a += string(s[i])
 			}
