1677b50db06ac6c10e4c0259dc95cdba7f8ca437
Guest User <Guest@TCB-17.local>
2025-01-30 00:23:19 +0500

    olol

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for _, letter := range s {
-		if letter < 65 || (letter > 90 && letter < 97) || letter > 122 {
+		if letter < 32 || letter > 126 {
 			return false
 		}
 	}
