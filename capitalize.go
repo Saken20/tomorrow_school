901dea2a98e1bdd889e5a8933fdfe09065c40e5f
Guest User <Guest@TCB-17.local>
2025-01-30 03:24:29 +0500

    loo

--- capitalize.go
+++ capitalize.go
@@ -8,7 +8,7 @@ func Capitalize(s string) string {
 		for i := 1; i < len(s); i++ {
 			if (s[i-1] >= 48 && s[i-1] <= 57) || (s[i-1] >= 65 && s[i-1] <= 90) || (s[i-1] >= 97 && s[i-1] <= 122) {
 				a += ToLower(string(s[i]))
-			} else if (s[i-1] < 48 && s[i-1] > 57) || (s[i-1] < 65 && s[i-1] > 90) || (s[i-1] < 97 && s[i-1] > 122) {
+			} else if s[i-1] < 48 && s[i-1] > 57 && s[i-1] < 65 && s[i-1] > 90 && s[i-1] < 97 && s[i-1] > 122 {
 				a += ToUpper(string(s[i]))
 			} else {
 				a += string(s[i])
