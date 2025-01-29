5809752e6b1660b7316763f3d18722638f1a579d
Guest User <Guest@TCB-17.local>
2025-01-30 01:15:15 +0500

    plol

---
+++ printnbrinorder.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n < 0 {
+		return
+	}
+	var t []int
+	if n == 0 {
+		t = append(t, 0)
+	}
+	for n > 0 {
+		t = append(t, n%10)
+		n /= 10
+	}
+	SortIntegerTable(t)
+	for _, val := range t {
+		z01.PrintRune(rune(val) + '0')
+	}
+}
+func SortIntegerTable(table []int) {
+	for i := 0; i < len(table); i++ {
+		for j := i + 1; j < len(table); j++ {
+			if table[j] < table[i] {
+				table[j], table[i] = table[i], table[j]
+			}
+		}
+	}
+}
