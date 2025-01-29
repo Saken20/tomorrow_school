96402005382b5df8323326bdf426c4315b74acc5
Guest User <Guest@TCB-17.local>
2025-01-30 03:25:24 +0500

    loo

--- capitalize.go
+++ capitalize.go
@@ -6,9 +6,9 @@ func Capitalize(s string) string {
 		return a
 	} else {
 		for i := 1; i < len(s); i++ {
-			if (s[i-1] >= 48 && s[i-1] <= 57) || (s[i-1] >= 65 && s[i-1] <= 90) || (s[i-1] >= 97 && s[i-1] <= 122) {
+			if (s[i-1] >= '0' && s[i-1] <= '9') || (s[i-1] >= 'A' && s[i-1] <= 'Z') || (s[i-1] >= 'a' && s[i-1] <= 'z') {
 				a += ToLower(string(s[i]))
-			} else if s[i-1] < 48 && s[i-1] > 57 && s[i-1] < 65 && s[i-1] > 90 && s[i-1] < 97 && s[i-1] > 122 {
+			} else if s[i-1] < '0' && s[i-1] > '9' && s[i-1] < 'A' && s[i-1] > 'Z' && s[i-1] < 'a' && s[i-1] > 'z' {
 				a += ToUpper(string(s[i]))
 			} else {
 				a += string(s[i])
