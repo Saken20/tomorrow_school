97cb47c7ada1a4415b28762f1897dbbde8e53420
Guest User <Guest@TCD-14.local>
2025-01-30 23:51:45 +0500

    lol

---
+++ splitwhitespaces.go
@@ -0,0 +1,18 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var ans []string
+	var word string
+	for _, ch := range s {
+		if ch != ' ' {
+			word += string(ch)
+		} else {
+			ans = append(ans, word)
+			word = ""
+		}
+	}
+	if word != "" {
+		ans = append(ans, word)
+	}
+	return ans
+}
