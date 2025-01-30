d59b398bfdcdb116b6d7110e6cd1d05c879e541b
Guest User <Guest@TCD-14.local>
2025-01-30 21:13:07 +0500

    lolo

--- makerange.go
+++ makerange.go
@@ -1,8 +1,12 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	var n []int
+	if min > max {
+		return n
+	}
 	size_n := max - min
-	n := make([]int, size_n)
+	n = make([]int, size_n)
 	for i := 0; i < size_n; i++ {
 		n[i] = i + min
 	}
