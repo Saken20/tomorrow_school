8b06fc2ca7f702cdb111f9877a8272cb1be560f9
Guest User <Guest@TCA-15.local>
2025-02-03 20:33:20 +0500

    point

--- point/main.go
+++ point/main.go
@@ -1,28 +1,9 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "fmt"
 
 type point struct {
 	x, y int
-}
-
-func prns(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
-
-func prni(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	if n >= 10 {
-		prni(n / 10)
-	}
-	z01.PrintRune(rune(n%10 + '0'))
 }
 
 func setPoint(ptr *point) {
@@ -31,12 +12,7 @@ func setPoint(ptr *point) {
 }
 
 func main() {
-	points := point{}
-	setPoint(&points)
-
-	prns("x = ")
-	prni(points.x)
-	prns(", y = ")
-	prni(points.y)
-	prns('\n')
+	points := &point{}
+	setPoint(points)
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
