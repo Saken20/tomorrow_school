5a8c580543254af776c4c2d9006a951fe74baf77
azzz <a.zzz947367@gmail.com>
2025-02-06 17:31:39 +0500

    olo

--- map.go
+++ map.go
@@ -3,8 +3,8 @@ package piscine
 func Map(f func(int) bool, a []int) []bool {
 	tr := []bool{}
 
-	for i, char := range a {
-		tr[i] = f(int(char))
+	for _, char := range a {
+		tr = append(tr, f(char))
 	}
 
 	return tr
